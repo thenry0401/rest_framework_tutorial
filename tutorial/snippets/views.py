2cc022d2b65aa180636dda2955ad6819054c52c5
thenry0401 <thenry0401@gmail.com>
2017-07-02 17:25:14 +0900

    permissions.py 파일 생성

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions
 
@@ -19,7 +20,10 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
 
 
 class UserList(generics.ListAPIView):
