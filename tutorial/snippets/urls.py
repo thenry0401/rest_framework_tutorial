e563d783618c0c55c421a309bc2e6765670eb7b4
thenry0401 <thenry0401@gmail.com>
2017-07-02 17:10:44 +0900

    url에 api-auth 추가

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
@@ -8,6 +9,8 @@ urlpatterns = [
 
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
