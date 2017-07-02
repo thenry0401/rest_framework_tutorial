fc2a879c7fea2e5b5e3624c1715295b46534bab1
thenry0401 <thenry0401@gmail.com>
2017-07-02 18:50:36 +0900

    router 사용하여 urls.py 리팩터링

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,44 +1,16 @@
 from django.conf.urls import url
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from snippets import views
 from django.conf.urls import include
 
-from snippets.views import SnippetViewSet, UserViewSet, api_root
-
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-
-    url(r'^snippets/$', snippet_list, name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    url(r'^snippets/(?P<pj>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 
 
-])
+# 라우터를 생성하고 뷰셋을 등록합니다
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-urlpatterns += [
+urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
